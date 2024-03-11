0f166501cf62ffd222aee93fdf31330775179471
AravindBalaji1990 <aravindbalaji.balaji7@gmail.com>
2024-03-11 20:28:31 +0300

    new commit

--- ExceptionHandlingDemo1.js
+++ ExceptionHandlingDemo1.js
@@ -5,7 +5,8 @@ var a= ["34","32","5","31","24","44","67"];
 console.log(a); 
 //b is undefined but still trying to fetch its value. Thus catch block will be invoked     
 console.log(c); 
+
 }catch(xyz){  
-console.log(b); 
+
 console.error("There is error which shows -> "+xyz.message); //Handling error  
 }  
